5202f673acbca653bd453c3b947644aa0f6699a0
Ravindra <pinjanravindra@gmail.com>
2018-12-30 15:02:04 +0530

    Add readme file

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'pawanacamping';
+  names = [{name:"Ravindra",comment:"This guys are very particular about their promises......",post:"CEO"},{name:"Ravindra",comment:"This guys are very particular about their promises......",post:"CEO"},{name:"Ravindra",comment:"This guys are very particular about their promises......",post:"CEO"}]
+}
